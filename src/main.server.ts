99ba19d472673c93237614de98da3fb9f7721f08
Nguyễn Huy Khải <66985115+nguyenhuykhai@users.noreply.github.com>
2024-12-07 03:23:39 +0700

    Commit

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,14 @@
+// Kiểm tra và tạo đối tượng global nếu chưa tồn tại
+if (typeof global === 'undefined') {
+  globalThis.global = globalThis;
+}
+
+// Các import của Angular và các cấu hình khác
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
+// Khởi tạo ứng dụng Angular SSR
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
 export default bootstrap;
